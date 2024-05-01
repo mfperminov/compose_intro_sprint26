0208b65d011fa8d5c3475e6ad8a8f046f687c44d
mfperminov <mfperminov@yandex-team.ru>
2024-05-01 20:09:20 +0300

    track_3

--- app/src/main/java/com/mperminov/compose_intro_sprint26/playlist/Track.kt
+++ app/src/main/java/com/mperminov/compose_intro_sprint26/playlist/Track.kt
@@ -1,12 +1,17 @@
 package com.mperminov.compose_intro_sprint26.playlist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,13 +19,21 @@ import com.mperminov.compose_intro_sprint26.R
 
 @Composable
 fun Track() {
-    Row {
+    Row(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = painterResource(id = R.drawable.queen_hits),
             contentDescription = "queen_greatest_hits",
-            modifier = Modifier.size(56.dp)
+            modifier = Modifier
+                .size(56.dp)
+                .clip(RoundedCornerShape(4.dp))
         )
-        Column {
+        Column(
+            modifier = Modifier.padding(start = 16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
 
             Text(text = "Bohemian Rhapsody")
             Text(text = "Queen")
