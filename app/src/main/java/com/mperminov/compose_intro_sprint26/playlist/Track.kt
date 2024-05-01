e50c31bfed5a686bbc7c60bbea9f5fae19f0557b
mfperminov <mfperminov@yandex-team.ru>
2024-05-01 19:56:42 +0300

    track_1

--- app/src/main/java/com/mperminov/compose_intro_sprint26/playlist/Track.kt
+++ app/src/main/java/com/mperminov/compose_intro_sprint26/playlist/Track.kt
@@ -1,8 +1,18 @@
 package com.mperminov.compose_intro_sprint26.playlist
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Track() {
+    Text(text = "Bohemian Rhapsody")
+    Text(text = "Queen")
+}
 
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun TrackPreview() {
+    Track()
 }
